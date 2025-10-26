bb13e834e6c62f88736c3d923d6301d9e3bf9cc8
Jack Tubby <jack200034@hotmail.com>
2025-10-26 15:40:42 +0000

    Start to add finnhub back in

--- main.go
+++ main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"log"
+	"fmt"
 	"time"
 	"stock-watch/clients/telegram"
+	"stock-watch/clients/finnhub"
 	"stock-watch/config"
 )
 
 func main() {
 	cfg := config.Load()
 	
+	finnhubClient := finnhub.NewClient(cfg.FinnhubApiKey)
 	telegramClient := telegram.NewClient(cfg.TelegramToken)
 	var lastOffset int
 
@@ -21,14 +24,20 @@ func main() {
 		
 		for _, update := range updates {
 			text := update.Message.Text
+			fmt.Println("text: ", text)
 			chatID := update.Message.Chat.Id
 			lastOffset = update.UpdateId + 1
 			
 			if text == "/start" {
 				telegramClient.SendMessage(chatID, "Welcome!")
 			}
-			if text == "hey" {
-				telegramClient.SendMessage(chatID, "You said 'hey'!")
+			if text == "quote" {
+				currentStatus, err := finnhubClient.GetQuote("AAPL")
+				if err != nil {
+					log.Fatal(err)
+				}
+				fmt.Println(currentStatus)
+				// telegramClient.SendMessage(chatID, currentStatus)
 			}
 		}
 		
